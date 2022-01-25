37c1a4c267e85dca1aee175cd578603e4f0d94c4
Areska <areskul@areskul.com>
2022-01-25 18:32:06 +0100

    added command line interface (vite like)

--- src/commands/main.ts
+++ src/commands/main.ts
@@ -1,2 +1,15 @@
 import { useConfig } from "@composables/config";
+import { cac } from "cac";
+
 const config = useConfig();
+const cli = cac("simp");
+
+cli
+  .option("-c, --config <file>", `[string] use specified config file`)
+  .option("--base <path>", `[string] public base path (default: /)`)
+  .option("-l, --logLevel <level>", `[string] info | warn | error | silent`)
+  .option("--clearScreen", `[boolean] allow/disable clear screen when logging`)
+  .option("-d, --debug [feat]", `[string | boolean] show debug logs`)
+  .option("-f, --filter <filter>", `[string] filter debug logs`)
+  .option("-m, --mode <mode>", `[string] set env mode`);
+cli.help();
