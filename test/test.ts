0faf2bba52ff1f1dcd267fa8ab6e3cde1fe1e5fb
Areska <areskul@areskul.com>
2022-01-27 19:26:25 +0100

    subtrees

--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,5 @@
-import { useSimp } from "@/index";
+// import { useSimp } from "@/index";
+import { useSimp } from "simpcicd/src";
 import config from "@/../simp.config";
-// import { useSimp } from "simpcicd";
-// import config from "@/../simp.config";
 
 const cli = useSimp(config);
