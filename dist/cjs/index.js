675fabb9ed9fc4a4c43928c0a7ec1d6ed0870508
Areska <areskul@areskul.com>
2022-01-27 21:03:53 +0100

    good build and json config attached files

--- dist/cjs/index.js
+++
@@ -1,1 +0,0 @@
-'use strict';Object.defineProperty(exports,'__esModule',{value:true});exports.useSimp=void 0;const cli_1=require('./src/composables/cli');const picocolors_1=require('picocolors');const useSimp=config=>{console.log((0,picocolors_1.blue)('SimpCICD'));const cli=(0,cli_1.useCli)(config);return cli;};exports.useSimp=useSimp;
