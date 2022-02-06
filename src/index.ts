33c194f2e64ffaa78e15b9a032959fdc9273a9cb
Areska <areskul@areskul.com>
2022-02-06 20:31:54 +0100

    export fork and call

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,16 @@ import { defineConfig, useConfig } from "@composables/config";
 import { useHooks } from "@resolvers/hooks";
 import { useTrigger } from "@resolvers/trigger";
 import { useExec } from "@composables/exec";
+import { fork } from "@utils/forker";
+import { call } from "@utils/caller";
 
-export { defineConfig, useConfig, useExec, useTrigger, useHooks, useCli };
+export {
+  defineConfig,
+  useConfig,
+  useExec,
+  useTrigger,
+  useHooks,
+  useCli,
+  fork,
+  call
+};
