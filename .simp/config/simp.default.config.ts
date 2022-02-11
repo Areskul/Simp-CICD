1c7ed0719da61afb0fb2432dd28c351db5836260
Areska <areskul@areskul.com>
2022-02-11 19:18:26 +0100

    chmod forker

--- .simp/config/simp.default.config.ts
+++ .simp/config/simp.default.config.ts
@@ -13,7 +13,7 @@ const defaultConfig = {
         },
         {
           name: "bin files mode",
-          commands: ["chmod -R +x dist/bin/*.js"]
+          commands: ["chmod +x dist/bin/*.js", "chmod +x dist/bin/forker/*.js"]
         }
       ],
       trigger: {
